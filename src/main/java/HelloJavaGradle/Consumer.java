bcb81c5e0ed59c883a6746ddbdec4fd3507b2094
imranansari <imran.iansari@gmail.com>
2020-05-05 06:31:17 -0700

    initial commit

---
+++ src/main/java/HelloJavaGradle/Consumer.java
@@ -0,0 +1,13 @@
+package HelloJavaGradle;
+
+public class Consumer {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
